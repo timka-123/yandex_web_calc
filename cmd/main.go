344c784a00d42e4b3f261f047a8b4e0f592d27b7
timka <git@timka.su>
2024-12-17 09:17:09 +0300

    add log on server startup

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"yandex_web_calc/internal"
 )
 
 func main() {
 	http.HandleFunc("/api/v1/calculate", internal.PanicMiddleware(internal.CalculatorMiddleware(internal.CalculatorHandler)))
+	log.Println("Starting server on 8080 port")
 	http.ListenAndServe(":8080", nil)
 }
