1e9eace4d7c328aff335ec6bf93259c6ca7861de
timka <git@timka.su>
2024-12-22 17:55:24 +0300

    rewrite some logic: replaced request data checker to handler; add http method checker to middleware

--- internal/handlers.go
+++ internal/handlers.go
@@ -9,7 +9,23 @@ import (
 
 func CalculatorHandler(w http.ResponseWriter, r *http.Request) {
 	var requestData CalculatorRequest
-	json.NewDecoder(r.Body).Decode(&requestData)
+	err := json.NewDecoder(r.Body).Decode(&requestData)
+
+	if err != nil {
+		log.Printf("Handled invalid request to %s. Maybe data was mailformed\n", r.URL)
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		response := CalculatorFailureResponse{Error: "Invalid request"}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	if requestData.Expression == "" {
+		log.Printf("Handled invalid request to %s. Expression %s was bad\n", r.URL, requestData.Expression)
+		response := CalculatorFailureResponse{Error: "Expression is required"}
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		json.NewEncoder(w).Encode(response)
+		return
+	}
 
 	result, err := pkg.Calc(requestData.Expression)
 	if err != nil {
--- internal/middlewares.go
+++ internal/middlewares.go
@@ -1,9 +1,7 @@
 package internal
 
 import (
-	"bytes"
 	"encoding/json"
-	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -28,27 +26,10 @@ func PanicMiddleware(next http.HandlerFunc) http.HandlerFunc {
 func CalculatorMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		r.Header.Set("Content-Type", "application/json")
-		var requestData CalculatorRequest
-
-		// I wanted to make this code more beautiful but my beautiful version does not working:(
-		bodyBytes, _ := ioutil.ReadAll(r.Body)
-		r.Body.Close()
-		r.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
-
-		err := json.Unmarshal(bodyBytes, &requestData)
-
-		if err != nil {
-			log.Printf("Handled invalid request to %s. Maybe data was mailformed\n", r.URL)
-			w.WriteHeader(http.StatusUnprocessableEntity)
-			response := CalculatorFailureResponse{Error: "Invalid request"}
-			json.NewEncoder(w).Encode(response)
-			return
-		}
-
-		if requestData.Expression == "" {
-			log.Printf("Handled invalid request to %s. Expression %s was bad\n", r.URL, requestData.Expression)
-			response := CalculatorFailureResponse{Error: "Expression is required"}
-			w.WriteHeader(http.StatusUnprocessableEntity)
+		if r.Method != http.MethodPost {
+			log.Printf("Handled invalid request to %s. Method %s is not allowed\n", r.URL, r.Method)
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			response := CalculatorFailureResponse{Error: "Method not allowed"}
 			json.NewEncoder(w).Encode(response)
 			return
 		}
